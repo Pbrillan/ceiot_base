156dd10ddd3534b1d1afc3da45c6a5cfe8f16a5a
Pablo <pablobrillanti@gmail.com>
2022-08-18 01:55:07 +0000

    smart_autoregister lado dispositivo

--- esp8266-dht11-arduino/esp8266-dht11-arduino.ino
+++ esp8266-dht11-arduino/esp8266-dht11-arduino.ino
@@ -8,14 +8,15 @@ DHTesp dht;
 
 float humidity = 0.0;
 float temperature = 0.0;
-
+bool Registrado = 0 ;
+String DEVICE_ID ;
 
 
 void setup() {
   delay(10000);
 
   Serial.begin(115200);
-
+  delay(10000);
   Serial.println("ESP8266 DHT HTTP POST");
   
   dht.setup(ONE_WIRE_GPIO, DHTesp::DHT11);
@@ -43,12 +44,59 @@ void setup() {
   Serial.println("");
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
-
 }
 
 void loop() {
   // wait for WiFi connection
   if ((WiFi.status() == WL_CONNECTED)) {
+
+  if ((Registrado== 0)) {
+    Registrado = 1;
+    DEVICE_ID = WiFi.macAddress();
+    Serial.println(dht.getStatusString());
+    Serial.print("Registrando Dispositivo como : ");
+    Serial.print(DEVICE_ID);
+    Serial.print(" \n");
+    WiFiClient client;
+    HTTPClient http;
+    Serial.print("[HTTP] begin...\n");
+
+    http.begin(client,"http://" API_IP_PORT "/device");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    Serial.print("[HTTP] POST...\n");
+    // start connection and send HTTP header and body
+    String post("id=");
+    post += DEVICE_ID;
+    post += "&n=";
+    post += "ESP8266";
+    post += "&k=";
+    post += "12";
+    int httpCode = http.POST(post);
+
+
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s when connecting to %s\n", http.errorToString(httpCode).c_str(), API_IP_PORT);
+    }
+
+    http.end();
+  }
+
+
+
 
   Serial.println(dht.getStatusString());
   humidity    = dht.getHumidity();
@@ -59,8 +107,14 @@ void loop() {
   Serial.println(humidity);
 
 
+ 
+
+
     WiFiClient client;
     HTTPClient http;
+
+
+
 
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
